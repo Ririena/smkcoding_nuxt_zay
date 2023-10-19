3d5701aa9c7e8400730f8337c8958888958924e6
Ririena <142820574+Ririena@users.noreply.github.com>
2023-10-19 21:37:10 +0700

    Nuxt Config Updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,35 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],
+ imports: {
+  dirs: [
+    'composables', 'composables/*/index.{ts,js,mjs,mts}', 'composables/**'
+  ],
+ },
+// router: {
+//  routes: [
+//   {
+//     name: 'about',
+//     path: '/about',
+//     components: 'pages/About.vue'
+//   },
+//  ],
+// },
+
+router: {
+  routes: [
+    {
+      name: 'about',
+      path: '/about',
+      components: 'pages/About.vue'
+    }, {
+name: 'home',
+path: '/home',
+components: 'pages/Home.vue'
+    },
+  ],
+},
 
   pwa: {
     manifest: {
