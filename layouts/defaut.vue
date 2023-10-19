ee4f243cfec05dcca2b82c0d79d66cb3d0481a52
Ririena <142820574+Ririena@users.noreply.github.com>
2023-10-19 21:35:08 +0700

    Update Layout And Fix Double Components In Localhost

---
+++ layouts/defaut.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+        <Header />
+        <slot />
+        
+        <Footer/>
+    </section>
+</template>
+
+<script>
+export default {
+    setup() {
+        definePageMeta({
+            layout: 'alternate-layout'
+       });
+    }
+}
+</script>
