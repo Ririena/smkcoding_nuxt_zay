201370463bbaf2e904d449184fcd76c8cb84da31
Ririena <142820574+Ririena@users.noreply.github.com>
2023-10-19 21:37:53 +0700

    Create About.vue

---
+++ pages/About.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+    <!--Card 1-->
+    <div class="rounded overflow-hidden shadow-lg">
+      <img class="w-full " src="~/assets/Adlin.jpg" alt="Gambar Saya">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">User</div>
+        <p class="text-gray-700 text-base">
+       Adlin Qaidul Jaisy, X-RPL-2, SMKN 7 Baleendah, Nuxt Development with Vue 
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#User</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Admin</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Junior Developer</span>
+      </div>
+    </div>
+    <!--Card 2-->
+    <div class="rounded overflow-hidden shadow-lg">
+      <img class="w-full object-fill" src="~/assets/vion.png" alt="tet">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Duivion</div>
+        <p class="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Logo</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Organisasi</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Test</span>
+      </div>
+    </div>
+
+    <!--Card 3-->
+    <div class="rounded overflow-hidden shadow-lg">
+      <img class="w-full object-fill" src="~/assets/Nuxt_logo_(2021).svg.png" alt="Photo Nuxt">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Programming Nuxt</div>
+        <p class="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#BelajarProgramming</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Bahasa Pemrograman</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Framework</span>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+
