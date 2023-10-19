fc9d2f83ac9e04c0a11c66b0bee00a9d19e020e8
Ririena <142820574+Ririena@users.noreply.github.com>
2023-10-19 21:33:25 +0700

    Updated Navbar

---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template class="scroll-smooth">
+    <header class="w-full border-b border-slate-200 py-6 bg-slate-200 scroll-smooth shadow-lg">
+        <div class="container">
+            <div class="flex justify-between items-center">
+                <div>
+                    <NuxtLink to="" class="text-3xl font-mono ml-4">DUIVION</NuxtLink>
+                </div>
+                <nav class="flex items-center gap-7 mr-7">
+                    <NuxtLink to="/home" class="text-base hover:text-2xl active:text-red-500 ">Home</NuxtLink>
+                    <NuxtLink to="/about" class="text-base hover:text-2xl active:text-blue-400">About</NuxtLink>
+                    <NuxtLink to="/sign" class="text-base hover:text-2xl">Sign Up</NuxtLink>
+                </nav>
+            </div>
+        </div>
+    </header>
+    
+</template>
