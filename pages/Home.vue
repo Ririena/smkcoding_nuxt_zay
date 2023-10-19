7464ce0582d41faf212dd14fea7c0ea6ce3b8984
Ririena <142820574+Ririena@users.noreply.github.com>
2023-10-19 21:38:18 +0700

    Create Home.vue

---
+++ pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <div class="">
+        <img src="~/assets/Adlin.jpg" class="object-cover h-36 w-36 rounded-full bg-slate-100 mx-auto mt-4 mb-4">
+        <h1 class="text-center mx-auto text-base font-mono text-lg">Nama: Adlin Qaidul Jaisy</h1>
+        <h1 class="text-center mx-auto text-base font-mono text-lg">Kelas: X RPL-2</h1>
+        <h1 class="text-center mx-auto text-base font-mono text-lg">Asal Dari: SMKN 7 Baleendah </h1>
+        <div class="flex justify-between items-center max-w-sm border border-slate-800 rounded-xl mx-auto shadow-md font-serif p-5 ">
+<NuxtLink class="mx-auto text-center content-center" to="www.github.com/Ririena" no-rel>Link Tree</NuxtLink>
+</div>
+       </div>
+      <div class="mb-20"></div>
+    </div>
+</template>
