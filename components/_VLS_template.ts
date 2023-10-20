f58239ee1373f680a78360d42486fe34d173a213
Ririena <142820574+Ririena@users.noreply.github.com>
2023-10-20 08:57:44 +0700

    Fix Bug Layouts

---
+++ components/_VLS_template.ts
@@ -0,0 +1,149 @@
+import { __VLS_internalComponent, __VLS_componentsOption, __VLS_name } from './Header.vue';
+
+function __VLS_template() {
+let __VLS_ctx!: InstanceType<__VLS_PickNotAny<typeof __VLS_internalComponent, new () => {}>> & {};
+/* Components */
+let __VLS_otherComponents!: NonNullable<typeof __VLS_internalComponent extends { components: infer C; } ? C : {}> & typeof __VLS_componentsOption;
+let __VLS_own!: __VLS_SelfComponent<typeof __VLS_name, typeof __VLS_internalComponent & (new () => { $slots: typeof __VLS_slots; })>;
+let __VLS_localComponents!: typeof __VLS_otherComponents & Omit<typeof __VLS_own, keyof typeof __VLS_otherComponents>;
+let __VLS_components!: typeof __VLS_localComponents & __VLS_GlobalComponents & typeof __VLS_ctx;
+/* Style Scoped */
+type __VLS_StyleScopedClasses = {};
+let __VLS_styleScopedClasses!: __VLS_StyleScopedClasses | keyof __VLS_StyleScopedClasses | (keyof __VLS_StyleScopedClasses)[];
+/* CSS variable injection */
+/* CSS variable injection end */
+let __VLS_resolvedLocalAndGlobalComponents!: {} &
+__VLS_WithComponent<'NuxtLink', typeof __VLS_localComponents, "NuxtLink", "NuxtLink", "NuxtLink">;
+({} as __VLS_IntrinsicElements).header; ({} as __VLS_IntrinsicElements).header;
+({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div;
+__VLS_components.NuxtLink; __VLS_components.NuxtLink; __VLS_components.NuxtLink; __VLS_components.NuxtLink; __VLS_components.NuxtLink; __VLS_components.NuxtLink; __VLS_components.NuxtLink; __VLS_components.NuxtLink;
+// @ts-ignore
+[NuxtLink, NuxtLink, NuxtLink, NuxtLink, NuxtLink, NuxtLink, NuxtLink, NuxtLink,];
+({} as __VLS_IntrinsicElements).nav; ({} as __VLS_IntrinsicElements).nav;
+({} as __VLS_IntrinsicElements).a; ({} as __VLS_IntrinsicElements).a;
+{
+const __VLS_0 = ({} as __VLS_IntrinsicElements)["header"];
+const __VLS_1 = __VLS_elementAsFunctionalComponent(__VLS_0);
+({} as __VLS_IntrinsicElements).header;
+({} as __VLS_IntrinsicElements).header;
+const __VLS_2 = __VLS_1({ ...{}, class: ("w-full border-b border-slate-200 py-6 bg-slate-200"), }, ...__VLS_functionalComponentArgsRest(__VLS_1));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_0, typeof __VLS_2> & Record<string, unknown>) => void)({ ...{}, class: ("w-full border-b border-slate-200 py-6 bg-slate-200"), });
+const __VLS_3 = __VLS_pickFunctionalComponentCtx(__VLS_0, __VLS_2)!;
+let __VLS_4!: __VLS_NormalizeEmits<typeof __VLS_3.emit>;
+{
+const __VLS_5 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_6 = __VLS_elementAsFunctionalComponent(__VLS_5);
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_7 = __VLS_6({ ...{}, class: ("container"), }, ...__VLS_functionalComponentArgsRest(__VLS_6));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_5, typeof __VLS_7> & Record<string, unknown>) => void)({ ...{}, class: ("container"), });
+const __VLS_8 = __VLS_pickFunctionalComponentCtx(__VLS_5, __VLS_7)!;
+let __VLS_9!: __VLS_NormalizeEmits<typeof __VLS_8.emit>;
+{
+const __VLS_10 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_11 = __VLS_elementAsFunctionalComponent(__VLS_10);
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_12 = __VLS_11({ ...{}, class: ("flex justify-between items-center"), }, ...__VLS_functionalComponentArgsRest(__VLS_11));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_10, typeof __VLS_12> & Record<string, unknown>) => void)({ ...{}, class: ("flex justify-between items-center"), });
+const __VLS_13 = __VLS_pickFunctionalComponentCtx(__VLS_10, __VLS_12)!;
+let __VLS_14!: __VLS_NormalizeEmits<typeof __VLS_13.emit>;
+{
+const __VLS_15 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_16 = __VLS_elementAsFunctionalComponent(__VLS_15);
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_17 = __VLS_16({ ...{}, }, ...__VLS_functionalComponentArgsRest(__VLS_16));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_15, typeof __VLS_17> & Record<string, unknown>) => void)({ ...{}, });
+const __VLS_18 = __VLS_pickFunctionalComponentCtx(__VLS_15, __VLS_17)!;
+let __VLS_19!: __VLS_NormalizeEmits<typeof __VLS_18.emit>;
+{
+let __VLS_20!: 'NuxtLink' extends keyof typeof __VLS_ctx ? typeof __VLS_ctx.NuxtLink : (typeof __VLS_resolvedLocalAndGlobalComponents)['NuxtLink'];
+const __VLS_21 = __VLS_asFunctionalComponent(__VLS_20, new __VLS_20({ ...{}, to: (""), class: ("text-xl font-mono ml-4"), }));
+({} as { NuxtLink: typeof __VLS_20; }).NuxtLink;
+({} as { NuxtLink: typeof __VLS_20; }).NuxtLink;
+const __VLS_22 = __VLS_21({ ...{}, to: (""), class: ("text-xl font-mono ml-4"), }, ...__VLS_functionalComponentArgsRest(__VLS_21));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_20, typeof __VLS_22> & Record<string, unknown>) => void)({ ...{}, to: (""), class: ("text-xl font-mono ml-4"), });
+const __VLS_23 = __VLS_pickFunctionalComponentCtx(__VLS_20, __VLS_22)!;
+let __VLS_24!: __VLS_NormalizeEmits<typeof __VLS_23.emit>;
+(__VLS_23.slots!).default;
+}
+(__VLS_18.slots!).default;
+}
+{
+const __VLS_25 = ({} as __VLS_IntrinsicElements)["nav"];
+const __VLS_26 = __VLS_elementAsFunctionalComponent(__VLS_25);
+({} as __VLS_IntrinsicElements).nav;
+({} as __VLS_IntrinsicElements).nav;
+const __VLS_27 = __VLS_26({ ...{}, class: ("flex items-center gap-6"), }, ...__VLS_functionalComponentArgsRest(__VLS_26));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_25, typeof __VLS_27> & Record<string, unknown>) => void)({ ...{}, class: ("flex items-center gap-6"), });
+const __VLS_28 = __VLS_pickFunctionalComponentCtx(__VLS_25, __VLS_27)!;
+let __VLS_29!: __VLS_NormalizeEmits<typeof __VLS_28.emit>;
+{
+let __VLS_30!: 'NuxtLink' extends keyof typeof __VLS_ctx ? typeof __VLS_ctx.NuxtLink : (typeof __VLS_resolvedLocalAndGlobalComponents)['NuxtLink'];
+const __VLS_31 = __VLS_asFunctionalComponent(__VLS_30, new __VLS_30({ ...{}, to: ("/home"), class: ("text-base hover:text-2xl active:text-red-500 "), }));
+({} as { NuxtLink: typeof __VLS_30; }).NuxtLink;
+({} as { NuxtLink: typeof __VLS_30; }).NuxtLink;
+const __VLS_32 = __VLS_31({ ...{}, to: ("/home"), class: ("text-base hover:text-2xl active:text-red-500 "), }, ...__VLS_functionalComponentArgsRest(__VLS_31));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_30, typeof __VLS_32> & Record<string, unknown>) => void)({ ...{}, to: ("/home"), class: ("text-base hover:text-2xl active:text-red-500 "), });
+const __VLS_33 = __VLS_pickFunctionalComponentCtx(__VLS_30, __VLS_32)!;
+let __VLS_34!: __VLS_NormalizeEmits<typeof __VLS_33.emit>;
+(__VLS_33.slots!).default;
+}
+{
+let __VLS_35!: 'NuxtLink' extends keyof typeof __VLS_ctx ? typeof __VLS_ctx.NuxtLink : (typeof __VLS_resolvedLocalAndGlobalComponents)['NuxtLink'];
+const __VLS_36 = __VLS_asFunctionalComponent(__VLS_35, new __VLS_35({ ...{}, to: ("/about"), class: ("text-base hover:text-2xl active:text-blue-400"), }));
+({} as { NuxtLink: typeof __VLS_35; }).NuxtLink;
+({} as { NuxtLink: typeof __VLS_35; }).NuxtLink;
+const __VLS_37 = __VLS_36({ ...{}, to: ("/about"), class: ("text-base hover:text-2xl active:text-blue-400"), }, ...__VLS_functionalComponentArgsRest(__VLS_36));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_35, typeof __VLS_37> & Record<string, unknown>) => void)({ ...{}, to: ("/about"), class: ("text-base hover:text-2xl active:text-blue-400"), });
+const __VLS_38 = __VLS_pickFunctionalComponentCtx(__VLS_35, __VLS_37)!;
+let __VLS_39!: __VLS_NormalizeEmits<typeof __VLS_38.emit>;
+(__VLS_38.slots!).default;
+}
+{
+let __VLS_40!: 'NuxtLink' extends keyof typeof __VLS_ctx ? typeof __VLS_ctx.NuxtLink : (typeof __VLS_resolvedLocalAndGlobalComponents)['NuxtLink'];
+const __VLS_41 = __VLS_asFunctionalComponent(__VLS_40, new __VLS_40({ ...{}, to: ("/sign"), class: ("text-base hover:text-2xl"), }));
+({} as { NuxtLink: typeof __VLS_40; }).NuxtLink;
+({} as { NuxtLink: typeof __VLS_40; }).NuxtLink;
+const __VLS_42 = __VLS_41({ ...{}, to: ("/sign"), class: ("text-base hover:text-2xl"), }, ...__VLS_functionalComponentArgsRest(__VLS_41));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_40, typeof __VLS_42> & Record<string, unknown>) => void)({ ...{}, to: ("/sign"), class: ("text-base hover:text-2xl"), });
+const __VLS_43 = __VLS_pickFunctionalComponentCtx(__VLS_40, __VLS_42)!;
+let __VLS_44!: __VLS_NormalizeEmits<typeof __VLS_43.emit>;
+(__VLS_43.slots!).default;
+}
+(__VLS_28.slots!).default;
+}
+(__VLS_13.slots!).default;
+}
+(__VLS_8.slots!).default;
+}
+(__VLS_3.slots!).default;
+}
+{
+const __VLS_45 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_46 = __VLS_elementAsFunctionalComponent(__VLS_45);
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_47 = __VLS_46({ ...{}, class: ("\u0077\u002d\u0031\u0030\u0020\u0068\u002d\u0031\u0030\u0020\u0062\u0067\u002d\u0070\u0069\u006e\u006b\u002d\u0032\u0030\u0030\u000d\u000a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0072\u006f\u0075\u006e\u0064\u0065\u0064\u002d\u0066\u0075\u006c\u006c\u0020\u000d\u000a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u000d\u000a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0066\u006c\u0065\u0078\u0020\u0066\u0069\u0078\u0065\u0064\u0020\u0062\u006f\u0074\u0074\u006f\u006d\u002d\u0035\u0020\u0072\u0069\u0067\u0068\u0074\u002d\u0035\u0020\u0063\u0075\u0072\u0073\u006f\u0072\u002d\u0070\u006f\u0069\u006e\u0074\u0065\u0072"), }, ...__VLS_functionalComponentArgsRest(__VLS_46));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_45, typeof __VLS_47> & Record<string, unknown>) => void)({ ...{}, class: ("\u0077\u002d\u0031\u0030\u0020\u0068\u002d\u0031\u0030\u0020\u0062\u0067\u002d\u0070\u0069\u006e\u006b\u002d\u0032\u0030\u0030\u000d\u000a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0072\u006f\u0075\u006e\u0064\u0065\u0064\u002d\u0066\u0075\u006c\u006c\u0020\u000d\u000a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u000d\u000a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0066\u006c\u0065\u0078\u0020\u0066\u0069\u0078\u0065\u0064\u0020\u0062\u006f\u0074\u0074\u006f\u006d\u002d\u0035\u0020\u0072\u0069\u0067\u0068\u0074\u002d\u0035\u0020\u0063\u0075\u0072\u0073\u006f\u0072\u002d\u0070\u006f\u0069\u006e\u0074\u0065\u0072"), });
+const __VLS_48 = __VLS_pickFunctionalComponentCtx(__VLS_45, __VLS_47)!;
+let __VLS_49!: __VLS_NormalizeEmits<typeof __VLS_48.emit>;
+{
+const __VLS_50 = ({} as __VLS_IntrinsicElements)["a"];
+const __VLS_51 = __VLS_elementAsFunctionalComponent(__VLS_50);
+({} as __VLS_IntrinsicElements).a;
+({} as __VLS_IntrinsicElements).a;
+const __VLS_52 = __VLS_51({ ...{}, href: ("#"), class: ("text-2xl m-auto"), }, ...__VLS_functionalComponentArgsRest(__VLS_51));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_50, typeof __VLS_52> & Record<string, unknown>) => void)({ ...{}, href: ("#"), class: ("text-2xl m-auto"), });
+const __VLS_53 = __VLS_pickFunctionalComponentCtx(__VLS_50, __VLS_52)!;
+let __VLS_54!: __VLS_NormalizeEmits<typeof __VLS_53.emit>;
+(__VLS_53.slots!).default;
+}
+(__VLS_48.slots!).default;
+}
+if (typeof __VLS_styleScopedClasses === 'object' && !Array.isArray(__VLS_styleScopedClasses)) {
+}
+var __VLS_slots!: {};
+return __VLS_slots;
+}
