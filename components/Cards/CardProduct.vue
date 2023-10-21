ef3023feed4a89975a245e80f45f922990bc95e4
Ririena <142820574+Ririena@users.noreply.github.com>
2023-10-21 12:04:19 +0700

    Product Card(WIP)

---
+++ components/Cards/CardProduct.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const props = defineProps({
+    product: {
+        type: Object,
+        default: {},
+    }
+})
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
