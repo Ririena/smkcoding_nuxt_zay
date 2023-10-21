62c80ccaa8258360254f7dc90450a6af46a71554
Ririena <142820574+Ririena@users.noreply.github.com>
2023-10-21 12:02:14 +0700

    Icon Plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,27 @@
+// import type { Config } from 'tailwindcss'
+// export default <Partial<Config>>{
+// theme: {
+// extend: {},
+// plugins: [require("daisyui")],
+// }
+// }
+
 import type { Config } from 'tailwindcss'
 export default <Partial<Config>>{
-theme: {
-extend: {},
-}
+ theme: {
+ extend: {
+ colors: {
+ primary: "#6867C5",
+ },
+ },
+ container: {
+ screens: {
+ sm: "470px",
+ md: "598px",
+ lg: "854px",
+ xl: "1110px",
+ },
+ center: true,
+ },
+ }
 }
