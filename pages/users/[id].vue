e2ed8eec147932decf972b561e3c41b38965855e
Ririena <142820574+Ririena@users.noreply.github.com>
2023-10-21 12:01:02 +0700

    Create [id].vue

---
+++ pages/users/[id].vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    
+  </div>
+  <span class="container">User ID: {{ $route.params.id }}</span>
+  <span>Route.name: {{ $route.name }}</span>
+</template>
+
+<script setup>
+const route = useRoute()
+const routeName = route.name;
+
+
+console.log(route.params) // { id: '123' }
+</script>
