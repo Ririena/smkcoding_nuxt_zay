bd439fac77557a305704d76b3c1334a5749414f2
Ririena <142820574+Ririena@users.noreply.github.com>
2023-10-22 22:34:24 +0700

    List Product

---
+++ composables/products.ts
@@ -0,0 +1,39 @@
+export const products = [
+    {
+      id: 1,
+      name: "Intel Core i5 10400",
+      category: "Processor",
+      price: 199,
+      image: "/images/products/1-2.png",
+      isCart: false,
+      description:"Merupakan Processeor Gen 10 yang memiliki 4 Core 8 Threads Serta sudah di Integrasi dengan IGPU yaitu UHD 630 atau setara dengan gt 1030 low profile",
+    },
+    {
+      id: 2,
+      name: "RTX 3050",
+      category: "Graphic Card",
+      price: 299,
+      image: "/images/products/1024.png",
+      isCart: false,
+      description: "Graphic Mid-End yang sudah memiliki fitur Ray Tracing serta DLSS 2.0 dan Tensor Core yang digunakan untuk entertainment AI supaya tidak berat dalam perenderan",
+    },
+    {
+      id: 3,
+      name: "Kingston Hyper X Furry 2x4",
+      category: "RAM",
+      price: 60,
+      image: "/images/products/kingstons.png",
+      isCart: false,
+      description: "RAM 2*4 GB dengan freekuensi kecepatan 2666mhz serta DDR4 ",
+    },
+    {
+      id: 4,
+      name: "WD BLACK",
+      category: "SSD",
+      price: 215,
+      image: "/images/products/wd-black.png",
+      isCart: false,
+      description: "SSD Yang sudah support M.2 Nvme Serta berkapasitas 1Terabytes dengan kecepatan hampir 3000mb/s read serta write 1200mb/s yang sudah support PCI 5.0",
+    },
+ 
+  ];
