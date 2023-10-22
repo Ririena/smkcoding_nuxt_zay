6633283ad86355d57e8cbfc157fc123922d72dd0
Ririena <142820574+Ririena@users.noreply.github.com>
2023-10-22 22:40:30 +0700

    Update [id].vue

--- pages/product/[id].vue
+++ pages/product/[id].vue
@@ -1,1 +1,11 @@
-
+<script lang="ts" setup>
+import { products } from "~/composables/constants/products";
+import { Products } from "~/types/products";
+const route = useRoute();
+const id = typeof route.params.id === 'string' ? parseInt(route.params.id) :
+null;
+const product = ref(products.find((item: Products) => item.id === id));
+</script>
+<template>
+<DetailProduct :product="product"/>
+</template>
