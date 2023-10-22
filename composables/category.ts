45c2bfc06d0ce22b536e6b825bc2c5535f1e38e7
Ririena <142820574+Ririena@users.noreply.github.com>
2023-10-22 22:38:56 +0700

    Create category.ts

---
+++ composables/category.ts
@@ -0,0 +1,23 @@
+
+export const category: Category[] = [
+    {
+    id: 1,
+    name: "Processor",
+    },
+    {
+    id: 2,
+    name: "Graphic Card",
+    },
+    {
+    id: 3,
+    name: "RAM",
+    },
+    {
+    id: 4,
+    name: "SSD",
+    },
+    ]
+    export interface Category {
+    id? : number,
+    name?: string,
+    }
