87059341f7450d1c3f6b2e1f404d91f3405dbf6b
Ririena <142820574+Ririena@users.noreply.github.com>
2023-10-22 22:00:19 +0700

    Component Card

--- components/Cards/CardProduct.vue
+++ components/Cards/CardProduct.vue
@@ -1,14 +1,33 @@
 <script lang="ts" setup>
 const props = defineProps({
-    product: {
-        type: Object,
-        default: {},
-    }
-})
+product: {
+type: Object,
+default: {},
+}
+});
 </script>
 
 <template>
-    <div>
-        
-    </div>
-</template>
+<section class="bg-white shadow-xl rounded-xl overflow-hidden ">
+<div :class="`w-full h-[200px] p-5  bg-gradient-to-r from-cyan-500 to-blue-500 bg-gradient-to-l hover:bg-gradient-to-tr`">
+<img :src="props.product.image" class="w-full h-full object-contain"/>
+</div>
+<div class="px-5 pb-5 pt-9 relative">
+
+<NuxtLink :to="`/product/${props.product.id}`" ><h3 class="text-lg font-
+bold mb-4 text-limit limit-2">{{ props.product.name }}</h3></NuxtLink>
+
+<div class="flex justify-between items-center">
+<span class="text-sm font-normal">{{ props.product.category }}</span>
+<span class="text-sm font-normal">${{ props.product.price }}</span>
+</div>
+
+<div :class="`cursor-pointer absolute -top-5 right-7 w-[50px] h-[50px] bg-
+white shadow-xl rounded-full flex justify-center items-center hover:bg-blue-600
+
+hover:text-white transition duration-300`">
+<i class="ri-shopping-cart-2-line"></i>
+</div>
+</div>
+</section>
+</template> 
